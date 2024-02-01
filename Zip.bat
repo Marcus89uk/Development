d234d361eb5d2ebe07dc120cdef3860b8c1bdf29
Marcus <marcus89uk@outlook.com>
2024-02-01 03:38:51 +0000

    upload

---
+++ Zip.bat
@@ -0,0 +1,9 @@
+echo on
+
+
+Echo zipping...
+
+"C:\Program Files\7-Zip\7z.exe" a -tzip "File.zip" "Directory you want to save"
+
+echo Done!
+pause
---
+++ modconfig-Server-Template.lua
@@ -0,0 +1,86 @@
+---------------------------------------------------- Avorion Extended ----------------------------------------- 
+
+
+-- Client | workshopid = "2852420983", path = "D:\\SteamLibrary\\steamapps\\workshop\\content\\445220\\2852420983"},
+-- Server | {workshopid = "2087266240"}, -- 
+scriptCachingEnabled = false
+achievementsEnabled = true
+forceEnabling = true
+
+mods = 
+{
+------------------  Libs & Essential Mods ---------------------------------------------------------------
+    {workshopid = "1722652757"}, -- AzimuthLib - Library for modders                                    
+	{workshopid = "2207469437"}, -- Extra Ship Classes Core
+	{workshopid = "3145948647"}, -- Bubbets UIElements ReUpload
+	{workshopid = "1747899176"}, -- Laserzwei's Admin Toolbox
+	{workshopid = "1692998037"}, -- Laserzwei's simple asteroid respawn mod
+	{workshopid = "2227699309"}, -- Laserzwei's Respawn asteroids everywhere
+	{workshopid = "2227690793"}, -- Laserzwei's Asteroid respawning sectors notifier
+    {workshopid = "3042506675"}, -- Always Running Captain Commands
+	---------------    Avorion Extended Start -----------------------------------------------------------
+    {workshopid = "2918443067"}, -- Xavorion: eXtended Scripting Framework
+    {workshopid = "2992808396"}, -- XSF: Arms Generator
+    {workshopid = "2992808472"}, -- XSF: Fleet Generator
+    {workshopid = "2992808561"}, -- XSF: Sector Generator
+    {workshopid = "2923179923"}, -- Xavorion: eXtended Avorion
+    {workshopid = "2992808773"}, -- Xavorion: Class System
+    {workshopid = "2992808971"}, -- Xavorion: Combat AI
+    {workshopid = "2992808719"}, -- Xavorion: Docking
+    {workshopid = "2992809187"}, -- Xavorion: Encounters
+    {workshopid = "2992808843"}, -- Xavorion: Flight Physics
+    {workshopid = "2992808903"}, -- Xavorion: Formations
+    {workshopid = "2992809036"}, -- Xavorion: Mining
+    {workshopid = "2992809240"}, -- Xavorion: Sectors
+	{workshopid = "2992808642"}, -- Xavorion: Starter Ship
+    {workshopid = "2992809109"}, -- Xavorion: Weaponry
+-------------------- SDK Mods --------------------------------------------------------------------------
+    {workshopid = "2087266240"}, --	[2.0] - (SDK) Support Library - Shadow Lib
+	{workshopid = "2090446289"}, -- [2.0] - (SDK) System Upgrade - Commercial Refinery Bay
+	{workshopid = "2093389493"}, -- [2.0] - (SDK) System Upgrade - Military Turret Control System
+	{workshopid = "2625966423"}, -- [2.0] - (SDK) System Upgrade - Commercial Turret Control System
+	--{workshopid = ""}, --
+-------------------- Avorion Extended Optional Mods ----------------------------------------------------
+    {workshopid = "3140194498"}, -- XT: Global Faction Packs - NOTE: EDIT this
+    {workshopid = "3134809180"}, -- XT: Vanilla Resource Depot
+	--{workshopid = "3139262394"}, -- XT: Immersive Scanner
+    --{workshopid = ""}, -- 
+    --{workshopid = ""}, --
+    --{workshopid = ""}, -- 
+------------------  NEW Missions ---------------------------------------------------------------------
+    {workshopid = "2439444436"}, -- New Mission - Attack Research Base
+    {workshopid = "2133506910"}, -- New Mission - Destroy Prototype Battleship
+    {workshopid = "2379374382"}, -- New Mission - Wrecking Havoc
+    {workshopid = "2381708468"}, -- New Misson - Collect Pirate Bounty
+    {workshopid = "2445091865"}, -- Extra Available Missions
+    {workshopid = "2743848682"}, -- New Mission - Escort Civilian Transports
+    {workshopid = "2746663587"}, -- New Mission - Ambush Pirate Raiders
+    {workshopid = "2750680477"}, -- New Mission - Transfer Satellite
+    {workshopid = "2901149152"}, -- New Mission - Eradicate Xsotan Infestation
+------------------ QOL,HUD & small changes Mods --------------------------------------------------------
+    {workshopid = "2053583501"}, -- Torpedo Production
+	{workshopid = "2559722572"}, -- Turrets x4 2.0
+	{workshopid = "2853436262"}, -- Rifted Shop Restock Button
+    {workshopid = "1765619430"}, -- Galaxy Map QoL
+	{workshopid = "2130272263"}, -- GravyUI(1.1.0)
+	{workshopid = "1932609041"}, -- Clock
+    {workshopid = "1695671502"}, -- Laserzwei's Advanced Shipyard
+    {workshopid = "3145928568"}, -- Mining_Priority
+	{workshopid = "1722261398"}, -- Compass-like Gate Pixel Icons
+	{workshopid = "2565262170"}, -- 1.0 Orders and Looping [2.0+]
+	{workshopid = "1769379152"}, -- Resource Display
+	{workshopid = "1995604018"}, -- Ranks Mod
+	{workshopid = "2732715741"}, --  [2.0] - Gate Founder
+	{workshopid = "2160909353"}, -- Ranks Integration
+	{workshopid = "2319814040"}, -- Resource Depot Income
+    {workshopid = "2941426377"}, -- Extra Efficient Captain Operations
+    {workshopid = "2943435828"}, -- Extra Long Range Captain Operations
+    {workshopid = "2944669973"}, -- Extra Safe Captain Operations
+    {workshopid = "2778851215"}, -- Centralized Bulletin Board (by Madranis) vFixed
+    {workshopid = "2944949200"}, -- Lost Friend Improvements-Reupload
+	{workshopid = "2853185089"}, -- Rifted No Pickup Sounds
+	{workshopid = "2457780924"}, -- Ressources in sector
+	{workshopid = "3009796364"}, -- Universal Adventuring Companion (2.2) Modified
+	{workshopid = "3131751741"}, -- More Captain Spawns (100% chance)
+    {workshopid = "1807357787"} -- Avorion Commands Package
+}
