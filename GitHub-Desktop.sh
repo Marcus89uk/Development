4a633266209d13f430bca8ba610e58db66fabc4a
marcus <marcus89uk@outlook.com>
2024-05-30 19:14:57 +0100

    Updated

---
+++ GitHub-Desktop.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# Adding GiHub-Desktop
+
+wget -qO - https://apt.packages.shiftkey.dev/gpg.key | gpg --dearmor | sudo tee /usr/share/keyrings/shiftkey-packages.gpg > /dev/null
+sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/shiftkey-packages.gpg] https://apt.packages.shiftkey.dev/ubuntu/ any main" > /etc/apt/sources.list.d/shiftkey-packages.list'
+apt update
+apt install github-desktop
--- Install-Main.sh
+++ Install-Main.sh
@@ -1,29 +1,57 @@
 #!/bin/sh
 
+
+# Updates before Install
+
+apt update
+apt upgrade -y
+apt autoremove -y
+apt autoclean -y
+
+# Installing Nvidia Drivers
+
+apt install nvidia-dkms-550 -y
+apt install nvidia-driver-550 -y
+
 # Adding Grub Customizer
 
-
-add-apt-repository ppa:danielrichter2007/grub-customizer
+add-apt-repository ppa:danielrichter2007/grub-customizer -y
 apt update
-apt install grub-customizer
-
-
+apt install -y grub-customizer
 
 # Adding Cubic
 
-apt-add-repository ppa:cubic-wizard/release
-apt install --no-install-recommends cubic
-
-
-
+apt-add-repository ppa:cubic-wizard/release -y
+apt install --no-install-recommends cubic -y
 
 # Adding GiHub-Desktop
 
-wget -qO - https://apt.packages.shiftkey.dev/gpg.key | gpg --dearmor | sudo tee /usr/share/keyrings/shiftkey-packages.gpg > /dev/null
-sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/shiftkey-packages.gpg] https://apt.packages.shiftkey.dev/ubuntu/ any main" > /etc/apt/sources.list.d/shiftkey-packages.list'
-apt update
-apt install github-desktop
+#wget -qO - https://apt.packages.shiftkey.dev/gpg.key | gpg --dearmor | sudo tee /usr/share/keyrings/shiftkey-packages.gpg > /dev/null
+#sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/shiftkey-packages.gpg] https://apt.packages.shiftkey.dev/ubuntu/ any main" > /etc/apt/#sources.list.d/shiftkey-packages.list'
+#apt update
+#apt install -y github-desktop
 
+# Installing Disks
 
+apt install gnome-disk-utility -y
 
+wget https://cdn.akamai.steamstatic.com/client/installer/steam.deb
+chmod 777 steam.deb
+apt install -y /steam.deb
+apt install steamcmd -y
 
+# Installing virtualbox
+
+apt install virtualbox -y
+
+# Installing Mainline
+
+sudo add-apt-repository ppa:cappelikan/ppa
+sudo apt update
+sudo apt install mainline -y
+
+#Mainline kernel Script
+
+wget https://raw.githubusercontent.com/pimlie/ubuntu-mainline-kernel.sh/master/ubuntu-mainline-kernel.sh
+chmod +x ubuntu-mainline-kernel.sh
+mv ubuntu-mainline-kernel.sh /usr/local/bin/
