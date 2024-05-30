4bd93b5cc3d47b34830537124a8735d2500e54a6
marcus <marcus89uk@outlook.com>
2024-05-30 17:04:35 +0100

    Create Install-Main.sh

---
+++ Install-Main.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# Adding Grub Customizer
+
+
+add-apt-repository ppa:danielrichter2007/grub-customizer
+apt update
+apt install grub-customizer
+
+
+# Adding GiHub-Desktop
+
+wget -qO - https://apt.packages.shiftkey.dev/gpg.key | gpg --dearmor | sudo tee /usr/share/keyrings/shiftkey-packages.gpg > /dev/null
+sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/shiftkey-packages.gpg] https://apt.packages.shiftkey.dev/ubuntu/ any main" > /etc/apt/sources.list.d/shiftkey-packages.list'
+apt update
+apt install github-desktop
+
+
+
+
