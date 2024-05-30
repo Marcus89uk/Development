949888939c49d671fe72625fc014fb9655fdb3c0
marcus <marcus89uk@outlook.com>
2024-05-30 17:33:40 +0100

    Update Install-Main.sh

--- Install-Main.sh
+++ Install-Main.sh
@@ -8,6 +8,15 @@ apt update
 apt install grub-customizer
 
 
+
+# Adding Cubic
+
+apt-add-repository ppa:cubic-wizard/release
+apt install --no-install-recommends cubic
+
+
+
+
 # Adding GiHub-Desktop
 
 wget -qO - https://apt.packages.shiftkey.dev/gpg.key | gpg --dearmor | sudo tee /usr/share/keyrings/shiftkey-packages.gpg > /dev/null
