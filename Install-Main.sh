8d0d06eefb31c6e368fb5c0776daf3d22e48ea3d
Marcus <marcus89uk@outlook.com>
2024-06-05 16:58:35 +0100

    Updated

--- Install-Main.sh
+++ Install-Main.sh
@@ -23,21 +23,38 @@ add-apt-repository ppa:danielrichter2007/grub-customizer -y
 add-apt-repository ppa:cubic-wizard/release -y
 add-apt-repository ppa:cappelikan/ppa -y
 add-apt-repository ppa:flatpak/stable -y
+add-apt-repository ppa:graphics-drivers/ppa -y
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys B05498B7
+sudo sh -c 'echo "deb [arch=amd64,i386 signed-by=/usr/share/keyrings/steam.gpg] http://repo.steampowered.com/steam/ stable steam" >> /etc/apt/sources.list.d/steam.list'
 
 #############################################################################################
 # Installing dependencies
 
 apt install wget -y
 apt install software-properties-common apt-transport-https ca-certificates gnupg2 -y
+apt install openjdk-17-jdk openjdk-17-jdk-headless openjdk-17-jre openjdk-21-jdk openjdk-21-jdk-headless openjdk-21-jre -y
 apt install curl -y
 apt install notepadqq -y
 apt install galternatives -y
 apt install flatpak -y
 apt install gnome-software-plugin-flatpak -y
+apt install gimp -y
+apt install gufw kgpg -y
+apt install synaptic -y
+
+# Installing Disks
+
+apt install gnome-disk-utility -y
 
 
+# Downloading Debs for install!
 
+#################################################################################################
+
+wget https://files.multimc.org/downloads/multimc_1.6-1.deb
+wget https://launcher.mojang.com/download/Minecraft.deb
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+wget https://piston.feed-the-beast.com/app/ftb-app-1.25.11-amd64.deb
 wget -qO - https://apt.packages.shiftkey.dev/gpg.key | gpg --dearmor | sudo tee /usr/share/keyrings/shiftkey-packages.gpg > /dev/null
 wget https://cdn.akamai.steamstatic.com/client/installer/steam.deb
 wget https://raw.githubusercontent.com/pimlie/ubuntu-mainline-kernel.sh/master/ubuntu-mainline-kernel.sh
@@ -57,11 +74,13 @@ chmod +x ubuntu-mainline-kernel.sh
 mv ubuntu-mainline-kernel.sh /usr/local/bin/
 
 ################################################################################################
+
 # Installing Web Browsers
 
 apt install konqueror -y
 apt install opera-stable -y
 apt install ./google-chrome-stable_current_amd64.deb -y
+
 
 # Installing Nvidia Drivers
 
@@ -74,6 +93,7 @@ apt install nvidia-driver-550 -y
 apt update
 apt install -y grub-customizer
 
+
 # Adding Cubic
 
 
@@ -82,29 +102,46 @@ apt install --no-install-recommends cubic -y
 # Adding GiHub-Desktop
 
 
-sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/shiftkey-packages.gpg] https://apt.packages.shiftkey.dev/ubuntu/ any main" > /etc/apt/sources.list.d/shiftkey-packages.list'
+
 apt update
 apt install -y github-desktop
 
-# Installing Disks
+# Installing Minecraft Launchers
 
-apt install gnome-disk-utility -y
+chmod 777 ftb-app-1.25.11-amd64.deb
+apt install -y ./ftb-app-1.25.11-amd64.deb
+chmod 777 Minecraft.deb
+apt install -y ./Minecraft.deb
+chmod 777 multimc_1.6-1.deb
+apt install ./multimc_1.6-1.deb -y
 
+# Installing Steam
 
-
+chmod 777 steam.deb
 apt install -y ./steam.deb && apt install steamcmd -y
 
 # Installing virtualbox
 
-apt install virtualbox -y
+apt install virtualbox virtualbox-dkms virtualbox-ext-pack virtualbox-guest-additions-iso virtualbox-guest-utils virtualbox-guest-x11 virtualbox-qt -y
+
 
 # Installing Mainline
 
 apt update
 apt install mainline -y
 
+apt autoremove -y
+apt autoclean -y
+
+
+
+dpkg-reconfigure debconf
+
+
 cd -
 
-ubuntu-mainline-kernel.sh -i
+rm -R -f installer
+
+#ubuntu-mainline-kernel.sh -i
 
 #echo"Finshed Install"
